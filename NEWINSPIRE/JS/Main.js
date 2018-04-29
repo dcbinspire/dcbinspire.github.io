a8f30016eeeb87a3503184f62600595cdc1a5023
Shenyi Cui <shenyicui@outlook.com>
2018-04-29 23:49:00 +0800

    Topic Module

---
+++ NEWINSPIRE/JS/Main.js
@@ -0,0 +1,12 @@
+var slideIndex = 0;
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("topicSlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 4000); 
+}
