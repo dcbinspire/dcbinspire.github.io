4d1e7fdb98f9cf4f0289ca6cf6d988e72fdf13c7
Shenyi Cui <shenyicui@outlook.com>
2018-05-05 15:05:57 +0800

    Challege Module Upload

--- NEWINSPIRE/JS/Main.js
+++ NEWINSPIRE/JS/Main.js
@@ -1,7 +1,7 @@
 var slideIndex = 0;
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("topicSlides");
+    var x = document.getElementsByClassName("challengeSlides");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none"; 
     }
